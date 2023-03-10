c6b1f9e4647ee5855e9ff36f87dd1dd5e8a7ae0b
DeybisMelendez <pixelweb2007@gmail.com>
2023-03-09 21:13:04 -0600

    primer release

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# horoscopo
+# Horóscopo
+
+Una sensilla pagina web que te da el horóscopo del día todos los días. Está creada desde cero utilizando solo html5, css y javascript.
+
+Consume la api [Aztro](https://aztro.readthedocs.io/en/latest/) por medio de la función fetch.
---
+++ index.js
@@ -0,0 +1,94 @@
+// API: https://aztro.readthedocs.io/en/latest/
+
+const SIGNS = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+const IMG = {
+    aries: "https://cdn-icons-png.flaticon.com/128/4684/4684270.png",
+    taurus: "https://cdn-icons-png.flaticon.com/128/4684/4684370.png",
+    aquarius: "https://cdn-icons-png.flaticon.com/128/4684/4684370.png",
+    leo: "https://cdn-icons-png.flaticon.com/128/4684/4684329.png",
+    cancer: "https://cdn-icons-png.flaticon.com/128/4684/4684304.png",
+    capricorn: "https://cdn-icons-png.flaticon.com/128/4684/4684314.png",
+    libra: "https://cdn-icons-png.flaticon.com/128/4684/4684334.png",
+    sagittarius: "https://cdn-icons-png.flaticon.com/128/4684/4684351.png",
+    scorpio: "https://cdn-icons-png.flaticon.com/128/4684/4684365.png",
+    gemini: "https://cdn-icons-png.flaticon.com/128/4684/4684319.png",
+    pisces: "https://cdn-icons-png.flaticon.com/128/4684/4684346.png",
+    virgo: "https://cdn-icons-png.flaticon.com/128/4684/4684376.png"
+}
+const QUERY_STRING = window.location.search
+const URL_PARAMS = new URLSearchParams(QUERY_STRING)
+const BUTTON_TOMORROW = document.getElementById("tomorrow")
+const BUTTON_TODAY = document.getElementById("today")
+const BUTTON_YESTERDAY = document.getElementById("yesterday")
+
+let day = URL_PARAMS.get("day") || "today"
+
+function createSections(sign) {
+    let section = document.createElement("section")
+    section.id = sign
+    document.getElementById("content").appendChild(section)
+}
+
+function getHoroscope(sign) {
+    let url = `https://aztro.sameerkumar.website/?sign=${sign}&day=${day}`
+    fetch(url, {
+        method: "POST"
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            let section = document.getElementById(sign)
+            section.innerHTML = `
+                <h2>${capitalize(sign)}</h2>
+                <img src="${IMG[sign]}">
+                <p>${json.date_range}</p>
+                <p class = "description">${json.description}</p>
+                <table>
+                    <tr>
+                        <th>Color</th>
+                        <th>Lucky Number</th>
+                        <th>Lucky Time</th>
+                        <th>Mood</th>
+                    </tr>
+                    <tr>
+                        <td>${json.color}</td>
+                        <td>${json.lucky_number}</td>
+                        <td>${json.lucky_time}</td>
+                        <td>${json.mood}</td>
+                    </tr>
+                </table>`
+        })
+}
+switch (day) {
+    case "tomorrow":
+        BUTTON_TOMORROW.disabled = true
+        break
+    case "today":
+        BUTTON_TODAY.disabled = true
+        break
+    case "yesterday":
+        BUTTON_YESTERDAY.disabled = true
+        break
+}
+
+SIGNS.forEach(sign => { createSections(sign); getHoroscope(sign) })
+
+
+// Button handler
+
+BUTTON_TODAY.onclick = function () {
+    window.location.href = "?day=today"
+}
+
+BUTTON_TOMORROW.onclick = function () {
+    window.location.href = "?day=tomorrow"
+}
+BUTTON_YESTERDAY.onclick = function () {
+    window.location.href = "?day=yesterday"
+}
+
+// Tools
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
